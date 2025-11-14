1b07b0e832201bdcff7a4eb1a9fd167fc052a3ef
Anmol Malhan <malhananmol1@gmail.com>
2025-11-14 22:05:17 +0530

    js file is still pending

--- script.js
+++ script.js
@@ -94,11 +94,34 @@ const playSong = id => {
     audio.currentTime = userData.songCurrentTime;
   }
   userData.currentSong = song;
-  playButton.classList.add("playing")
+  playButton.classList.add("playing");
   audio.play()
 }
+
+const pauseSong = () => {
+  userData.songCurrentTime = audio.currentTime;
+  playButton.classList.remove("playing");
+  audio.pause();
+}
+
+
+
 playButton.addEventListener("click", () => {
-    if (userData.currentSong === null) {
+  if (userData.currentSong === null) {
     playSong(userData.songs[0].id);
+  } else {
+    playSong(userData.currentSong.id);
   }
 });
+
+const songs = document.querySelectorAll(".playlist-song");
+
+songs.forEach((song) => {
+  const id = song.getAttribute("id").slice(5);
+  const songBtn = song.querySelector("button");
+  songBtn.addEventListener("click", () => {
+      playSong(Number(id));
+  })
+})
+
+pauseButton.addEventListener("click", pauseSong);
